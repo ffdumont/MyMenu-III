21c6ee24d9b2c1eecc2f4efdf27733ccde17cf83
Francois Dumont <francois.dumont1@gmail.com>
2018-12-29 22:24:11 +0100

    Added :
    runMenu()
    
    Given_ChikenInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess()

--- test/com/ocr/anthony/OrderTest.java
+++ test/com/ocr/anthony/OrderTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -25,6 +26,23 @@ public class OrderTest {
     }
 
     Order order = new Order();
+
+    @Test
+    public void Given_ChikenInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess() {
+
+        System.setIn(new ByteArrayInputStream("1\n".getBytes()));
+
+        order = new Order();
+
+        order.runMenu();
+
+        String output = outContent.toString().replace("\r\n", "\n");
+
+        assertEquals(output.endsWith("Vous avez choisi comme menu : poulet\n"), true);
+
+        assertEquals(output.length() > "Vous avez choisi comme menu : poulet\n".length(), true);
+
+    }
 
     @Test
     public void Given_Chicken_When_DisplayMenuSelected_Then_DisplayChickenSentence() {
